05d0c15d9c09a17b0d7e5fa2e2a2f09af9ffcb5c
Kevin Niland <G00342279@gmit.ie>
2020-10-24 19:30:59 +0100

    Conversion of original project to Spring Boot

---
+++ src/main/java/kevinniland/Asynchronous/Language/Detection/AsynchronousLanguageDetectionApplication.java
@@ -0,0 +1,13 @@
+package kevinniland.Asynchronous.Language.Detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousLanguageDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousLanguageDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kevinniland/Asynchronous/Language/Detection/AsynchronousLanguageDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinniland.Asynchronous.Language.Detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousLanguageDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
